fe08ca8da008fa3f19a37905c46e61736b7283ba
Jacob Drew <71591438+JakesMD@users.noreply.github.com>
2022-08-07 17:33:58 +0200

    Create color theme

---
+++ calla-app/lib/themes/color_theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+/// The class is a custom color theme that also contains the colors for light and dark mode.
+class MyColorTheme {
+  final Color text;
+  final Color background;
+
+  final Color green;
+  final Color orange;
+  final Color blue;
+  final Color purple;
+  final Color pink;
+
+  MyColorTheme({
+    required this.text,
+    required this.background,
+    required this.green,
+    required this.orange,
+    required this.blue,
+    required this.purple,
+    required this.pink,
+  });
+
+  factory MyColorTheme.light() => MyColorTheme(
+        text: const Color(0xFF292C3C),
+        background: const Color(0xFFFFFFFF),
+        green: const Color(0xFFCDE885),
+        orange: const Color(0xFFFFD182),
+        blue: const Color(0xFFAEFFE4),
+        purple: const Color(0xFFB3BAE8),
+        pink: const Color(0xFFFFBCF6),
+      );
+
+  // TODO: create dark mode colors.
+  factory MyColorTheme.dark() => MyColorTheme(
+        text: const Color(0xFF292C3C),
+        background: const Color(0xFFFFFFFF),
+        green: const Color(0xFFCDE885),
+        orange: const Color(0xFFFFD182),
+        blue: const Color(0xFFAEFFE4),
+        purple: const Color(0xFFB3BAE8),
+        pink: const Color(0xFFFFBCF6),
+      );
+}
